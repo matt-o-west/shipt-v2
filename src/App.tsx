6970221930c91e0e6b2b1247b7cfe4f28ea3fb5e
matt-o-west <matt.omalley.west@gmail.com>
2023-01-15 14:24:21 -0500

    working on cart logic and async thunk

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import { Routes, Route, BrowserRouter as Router, useParams } from 'react-router-dom'
+import { useSelector } from 'react-redux'
 import ProductPage from './components/ProductPage'
 import HomePage from './components/HomePage'
 import Nav from './components/Nav'
@@ -8,11 +9,12 @@ import Nav from './components/Nav'
 function App() {
   const [count, setCount] = useState(0)
   const params = useParams()
+  const cartTotal = useSelector((state) => state.cart.length)
 
   return (
     <div className='bg-blue-100'>
         <Router>
-        <Nav />
+        <Nav cartTotal={cartTotal} />
 
         <Routes>
           <Route path='/' element={<HomePage />} />
